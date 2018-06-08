a820f7af6f4bcbe04c43114dc098e8653053aaa6
alexandreyc <alexandre.crayssac@gmail.com>
2018-06-08 10:25:30 +0200

    Added method to deserialize BeaconSniffer from json

--- src/main/java/com/renault/datalake/openday/common/BeaconSniffer.java
+++ src/main/java/com/renault/datalake/openday/common/BeaconSniffer.java
@@ -1,8 +1,12 @@
 package com.renault.datalake.openday.common;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.beam.sdk.coders.AvroCoder;
 import org.apache.beam.sdk.coders.DefaultCoder;
 import org.joda.time.Instant;
+
+import java.io.IOException;
 
 @DefaultCoder(AvroCoder.class)
 public class BeaconSniffer {
@@ -14,5 +18,20 @@ public class BeaconSniffer {
 
     public String toJson() {
         return String.format(JSON_PATTERN, snifferAddr, advAddr, datetime.getMillis() / 1000.0);
+    }
+
+    public static BeaconSniffer fromJson(String json) {
+        try {
+            JsonNode obj = new ObjectMapper().readTree(json);
+            BeaconSniffer bs = new BeaconSniffer();
+
+            bs.snifferAddr = obj.get("sniffer_addr").asText();
+            bs.advAddr = obj.get("adv_addr").asText();
+            bs.datetime = new Instant((long) (obj.get("datetime").asDouble() * 1000L));
+
+            return bs;
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
